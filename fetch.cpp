75435d3c71f736ed330bc33fa2b6d11edc07b983
Esteban <estebanchampiongi@gmail.com>
2026-02-28 18:55:48 -0500

    FEAT: se termino de implementar la condicional para determinar la instruccion

--- fetch.cpp
+++ fetch.cpp
@@ -4,18 +4,49 @@
 
 using namespace std;
 
-vector<string> Fetch(string &line){
+void Fetch(string &line){
     vector<string> delimitador;
     istringstream iss(line);   // flujo de entrada desde la cadena
-    string palabra;
+    string auxPalabra;
+    int cnt = 0;
 
-    while (iss >> palabra){
-        delimitador.push_back(palabra);
+    while (iss >> auxPalabra && cnt < 4){
+        delimitador.push_back(auxPalabra);
+        cnt++;
 
+    } 
+    
+    // para determinar que instruccion realizar
 
+    string instruction = delimitador[0];
+
+    if (instruction == "SET"){
+        instructionSET(delimitador);
     }
-
-
+    else if (instruction == "LDR"){
+        instructionLDR(delimitador);
+    }
+    else if (instruction == "ADD"){
+        instructionADD(delimitador);
+    }
+    else if (instruction == "INC"){
+        instructionINC(delimitador);
+    }
+    else if (instruction == "DEC"){
+        instructionDEC(delimitador);
+    }
+    else if (instruction == "STR"){
+        instructionSTR(delimitador);
+    }
+    else if (instruction == "SHW"){
+        instructionSHW(delimitador);
+    }
+    else if (instruction == "PAUSE"){
+        instructionPAUSE(delimitador);
+    }
+    else if (instruction == "END"){
+        instructionEND(delimitador);
+    }
 
 
 
